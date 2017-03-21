2c597988e0edef183a7c217e629c72b71cef44b2
harsh438 <shah.harsh438@hotmail.com>
2017-03-21 11:19:33 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,71 +9,10 @@ configure_aws_cli(){
 	aws configure set default.output json
 }
  
-deploy_cluster() {
-
-    family="demo-staging-family"
-
-    make_task_def
-    register_definition
-    if [[ $(aws ecs update-service --cluster Staging-Cluster --service demo-staging --task-definition $revision | \
-                   $JQ '.service.taskDefinition') != $revision ]]; then
-        echo "Error updating service."
-        return 1
-    fi
-
-    # wait for older revisions to disappear
-    # not really necessary, but nice for demos
-    for attempt in {1..30}; do
-        if stale=$(aws ecs describe-services --cluster Staging-Cluster --services demo-staging | \
-                       $JQ ".services[0].deployments | .[] | select(.taskDefinition != \"$revision\") | .taskDefinition"); then
-            echo "Waiting for stale deployments:"
-            echo "$stale"
-            sleep 5
-        else
-            echo "Deployed!"
-            return 0
-        fi
-    done
-    echo "Service update took too long."
-    return 1
-}
-
-make_task_def(){
-	task_template='[
-		{
-			"name": "go-sample-webapp",
-			"image": "%s.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:%s",
-			"essential": true,
-			"memory": 200,
-			"cpu": 10,
-			"portMappings": [
-				{
-					"containerPort": 8080,
-					"hostPort": 80
-				}
-			]
-		}
-	]'
-	
-	task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CIRCLE_SHA1)
-}
-
 push_ecr_image(){
 	eval $(aws ecr get-login --region us-east-1)
 	docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/go-sample-webapp:$CIRCLE_SHA1
 }
 
-register_definition() {
-
-    if revision=$(aws ecs register-task-definition --container-definitions "$task_def" --family $family | $JQ '.taskDefinition.taskDefinitionArn'); then
-        echo "Revision: $revision"
-    else
-        echo "Failed to register task definition"
-        return 1
-    fi
-
-}
-
 configure_aws_cli
 push_ecr_image
-deploy_cluster
